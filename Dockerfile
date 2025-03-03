30aa1d01e2ecf90d9a169caf03f16ed8620c0550
siddharth Paul <siddharth.paul@zemosolabs.com>
2025-03-03 23:49:40 +0530

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
 # Use an official Node.js runtime as the base image.
-FROM node:16
+FROM node:22
 
 WORKDIR /app
 
-COPY src/* ./src/
 COPY package*.json ./
 RUN npm install
 
 COPY . .
 
 EXPOSE 8080
-CMD [ "node", "src/index.js" ]
+CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-exports.helloWorld = (req, res) => {
+const functions = require('@google-cloud/functions-framework');
+
+functions.http('hellowWorld',(req, res) => {
     // Access any query parameters or request body data as needed
     const name = req.query.name || req.body.name || "World";
   
     // Send a response
     res.status(200).send(`Welcome to harness, ${name}! This is a sample Node.js app on Google Cloud Functions.`);
-  };
+  });
   
