36baf1ff8e83a3eb918b8cc2fd19f776e6c7bf6c
siddharth Paul <siddharth.paul@zemosolabs.com>
2025-03-03 21:38:30 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+exports.helloWorld = (req, res) => {
+    // Access any query parameters or request body data as needed
+    const name = req.query.name || req.body.name || "World";
+  
+    // Send a response
+    res.status(200).send(`Welcome to harness, ${name}! This is a sample Node.js app on Google Cloud Functions.`);
+  };
+  
